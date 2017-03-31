646334ccea2c7aaffd5a577f8707605ba57c7aef
baedy91 <baedy91@gmail.com>
2017-03-31 17:53:59 +0900

    model

---
+++ JsonToWord/JsonToWord/Model/RootAllModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace JsonToWord.Model
+{
+    public class RootAllModel : INotifyPropertyChanged
+    {
+        #region MyProperty
+        private ObservableCollection<WordListModel> _root;
+        public ObservableCollection<WordListModel> Root
+        {
+            get { return _root; }
+            set
+            {
+                if (_root != value)
+                {
+                    _root = value;
+                    OnPropertyChanged("Root");
+                }
+            }
+        }
+        #endregion
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
