1ae70ef61f31a3134b670e16fc59575249b40835
baedy91 <baedy91@gmail.com>
2017-04-06 10:06:56 +0900

    Messenger

---
+++ JsonToWord/JsonToWord/Message/UpdateMsg.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JsonToWord.Model;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace JsonToWord.Message
+{
+    class UpdateMsg: NotificationMessageAction<RootAllModel>
+    {
+        public int index { get; set; }
+        public string updateData { get; set; }
+        public string oldData { get; set; }
+
+        public UpdateMsg(string notification, Action<RootAllModel> callback) 
+            : base(notification, callback)
+        {         
+        }
+    }
+}
